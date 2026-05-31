fe176fa9357d1b1cd197cb93e7ad3abe6ff752ee
SelfMadeSystem <sms@shoghisimon.ca>
2026-05-31 18:17:48 -0400

    update ishape

--- src/utils/shapeUtils.ts
+++ src/utils/shapeUtils.ts
@@ -5,10 +5,13 @@ import { Vector2 } from "./vec";
  * Creates a union shape from a list of polygons.
  */
 export function unionPolygons(polygons: Vector2[][]): Vector2[][][] {
-  const simplified: [number, number][][][] = simplify(
+  const simplified = simplify(
     polygons.map((p) => p.map((p) => p.a)),
     FillRule.NonZero,
   );
+  if (!simplified) {
+    throw new Error("Failed to simplify polygons");
+  }
   return simplified.map((shape) =>
     shape.map((polygon) => polygon.map(([x, y]) => new Vector2(x, y))),
   );
